98acbe069d2f1386f578fd5070aa73e8c84f4742
William Diakité <william.diakite@gmail.com>
2021-12-28 15:33:27 -0500

    adds first routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { fetchMeetings } from './calls';
+</script>
+
+
+<h1>Welcome to CBC</h1>
+
+{#await fetchMeetings()}
+  <p>Fetching data...</p>
+  {:then data}
+    {#each data.splice(0, 10) as meeting}
+      <div class='meeting'>
+        <span>{meeting.title}</span>
+        <span>{meeting.date}</span>
+        <ul>
+            {#each meeting.deliberations as deliberation}
+                <li> <a href={"deliberations/" + deliberation.id}>{deliberation.id}</a> - {deliberation.title} - {deliberation.commune}</li>
+            {/each}
+        </ul>
+      </div>
+    {/each}
+{/await}
+
+
+
